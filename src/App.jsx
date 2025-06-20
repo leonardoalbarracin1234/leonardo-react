14c00ada42f7711a799325b047533a5063da16cf
Leonardo <leo12albarracin@gmail.com>
2025-06-20 15:39:45 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Footer from "./assets/components/footer";
+
 export default function App(){
   return(
     <>
       <div>
       <h1>Que miras bobo</h1>
       <p>mi vida es un pesadillo con ismael arpi</p>
-      
+
       <Footer />
       </div>
     </>
